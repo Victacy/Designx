aaeda435c78dedb3cafd347d0b69b0132fa991d1
Victacy <74544333+Victacy@users.noreply.github.com>
2021-01-08 12:38:12 +0000

    profile working

--- server/index.js
+++ server/index.js
@@ -24,20 +24,14 @@ mongoose.connect(process.env.CONNECT_URI,config)
 .catch(err => {
     console.log("A problem occurred",err)
 })
-// serve -s build
+
 app.use(express.json())
 
 app.use(require('./routes/auth.js'))
 app.use(require('./routes/post.js'))
 app.use(require('./routes/user.js'))
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static('client/build'))
-    const path=require('path')
-    app.use("*",(req,res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+
 
 app.listen(port,() =>{
     console.log('Express is running on port',port)
